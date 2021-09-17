78dc109ecc62f9b3242f734a2d3c9411db8dab77
Gabriel Cirtea <gcirtea@yahoo.com>
2021-09-17 22:24:41 +0300

    Change port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
 
 // start server
-app.listen(process.env.PORT || 4000).then(({url}) => console.log(`Server listening on: ${url}`))
+app.listen(process.env.PORT || 3000).then(({url}) => console.log(`Server listening on: ${url}`))
 
